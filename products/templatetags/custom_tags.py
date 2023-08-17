d3e14bf2a6e0950d43e3958cf72d0f24a2b9228a
BasilZap <zapr9@yandex.ru>
2023-08-17 12:39:04 +0300

    Добавлены комментарии в файл custom_tags.py

--- products/templatetags/custom_tags.py
+++ products/templatetags/custom_tags.py
@@ -3,14 +3,16 @@ from django import template
 register = template.Library()
 
 
+# Фильтр обрезает описание после 100 символов
 @register.filter()
 def cut_text(val: str):
     if len(val) > 100:
-        return val[:100]
+        return f'{val[:100]}...'
     else:
         return val
 
 
+# Фильтр позволяет указывать путь к картинке в виде {{ object.image|mediapath }}
 @register.filter()
 def mediapath(val):
     if val:
@@ -19,6 +21,7 @@ def mediapath(val):
         return '#'
 
 
+# Тег позволяет указывать путь к картинке в виде {% mediapath object.image %}
 @register.simple_tag()
 def mediapath(val):
     if val:
