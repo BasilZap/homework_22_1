b336cdd965fa3d0a27cca205f133f6d851b90c9f
BasilZap <zapr9@yandex.ru>
2023-08-17 08:15:10 +0300

    Добавлена страница продукта, прописано отображение и urls, шаблоны разделены

--- products/views.py
+++ products/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from products.models import Category, Product
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Продукты'
+    }
+    return render(request, 'products/index.html', context)
+
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(id=pk),
+        'title': f'{product_item.product_name}'.capitalize()
+    }
+    return render(request, 'products/product.html', context)
