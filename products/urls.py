0ea08357256df353e1ff93f976e5f54e8e353329
BasilZap <zapr9@yandex.ru>
2023-08-21 19:20:24 +0300

    Контроллеры переведены из fbv в cbv, исправлен вывод отдельного продукта в product.html

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from products.views import index, product
+from products.views import ProductsListView, ProductDetailView
 from products.apps import ProductsConfig
 
 app_name = ProductsConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:pk>/product/', product, name='product'),
+    path('', ProductsListView.as_view(), name='index'),
+    path('<int:pk>/product/', ProductDetailView.as_view(), name='product'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
 from products.models import Product
+from django.views.generic import ListView, DetailView
 
 
-# Формируем отображение продуктов на странице index
-def index(request):
-    context = {
-        'object_list': Product.objects.all(),
-        'title': 'Продукты'
-    }
-    return render(request, 'products/index.html', context)
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/index.html'
 
 
-# Формируем отображение продукта на странице product
-def product(request, pk):
-    product_item = Product.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(id=pk),
-        'title': f'{product_item.product_name}'.capitalize()
-    }
-    return render(request, 'products/product.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product.html'
