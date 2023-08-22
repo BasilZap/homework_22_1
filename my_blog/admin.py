5903e398d2b9ccd8639a5e151118dd245863ace0
BasilZap <zapr9@yandex.ru>
2023-08-22 23:33:05 +0300

    Поле slug в модели изменено на необязательное, создана миграция, создана админка.

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from my_blog.models import MyBlog
 
-# Register your models here.
+
+# Настройка отображения для категорий в Админке
+@admin.register(MyBlog)
+class MyBlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'image', 'creation_date', 'is_published')
--- my_blog/models.py
+++ my_blog/models.py
@@ -6,7 +6,7 @@ NULLABLE = {'blank': True, 'null': True}
 # Описание модели Блог
 class MyBlog(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название статьи')
-    slug = models.CharField(max_length=200, verbose_name='Slug')
+    slug = models.CharField(max_length=200, verbose_name='Slug', **NULLABLE)
     body = models.TextField(verbose_name='Текст статьи')
     image = models.ImageField(upload_to='product/', verbose_name='Фото', **NULLABLE)
     creation_date = models.DateTimeField(verbose_name='Дата создания')
@@ -14,7 +14,7 @@ class MyBlog(models.Model):
     view_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
 
     def __str__(self):
-        return f'{self.title} {self.is_published} {self.view_count}'
+        return f'{self.title}'
 
     class Meta:
         verbose_name = 'Статья'
