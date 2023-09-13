0592a0452e5d046d6b0a1b58fa9b56cdb955eaab
BasilZap <zapr9@yandex.ru>
2023-09-14 01:21:09 +0300

    Сформированы контроллеры для отправки верификационного сообщения и обработки прехода пользователя по верификационной ссылке. Добавлен шаблон формы верификации verify_email.html

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from users.views import RegisterView, UserUpdateView, generate_password
+from users.views import RegisterView, UserUpdateView, generate_password, verify_email, get_verify
 from users.apps import UsersConfig
 
 
@@ -13,4 +13,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', UserUpdateView.as_view(), name='profile'),
     path('profile/pass_gen', generate_password, name='generate_password'),
+    path('profile/email_verify', get_verify, name='get_verify'),
+    path('register/verify', verify_email, name='verify_email'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,15 +6,18 @@ from users.models import User
 from django.views.generic import CreateView, UpdateView
 from config.settings import EMAIL_HOST_USER
 from random import randint
+from django.contrib import messages
 
 
+# Контроллер регистрации пользователя
 class RegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
-    success_url = reverse_lazy('products:login')
+    success_url = reverse_lazy('products:list')
     template_name = 'users/register.html'
 
 
+# Контроллер редактирования пользователя
 class UserUpdateView(UpdateView):
     model = User
     success_url = reverse_lazy('users:profile')
@@ -42,13 +45,64 @@ class UserUpdateView(UpdateView):
 
 
 def generate_password(request):
-    new_pass = ''.join(str(randint(0, 9)) for _ in range(9))
+    """
+    Контроллер генерации пароля пользователя и отправки его по email
+    """
+    new_pass = ''.join(str(randint(0, 9)) for _ in range(8))
     send_mail(
         subject='Смена пароля',
         message=f'Ваш новый пароль {new_pass}',
         from_email=EMAIL_HOST_USER,
         recipient_list=[request.user.email]
     )
+    # Запоминаем и сохраняем пароль
     request.user.set_password(new_pass)
     request.user.save()
-    return redirect(reverse('products:login'))
+    return redirect(reverse('users:login'))
+
+
+def verify_email(request):
+    """
+    Контроллер верификации пользователя перешедшего по ссылке
+    из email
+    """
+    # Получаем из get-запроса с формы pk и код
+    user_pk = request.GET.get('pk')
+    code = request.GET.get('code')
+    # Проверка что pk число для предотвращения ошибки
+    if user_pk.isdigit():
+        # Проверка того, что код и pk пользователя совпадают с данными текущего пользователя
+        if request.user.verify_code == code and request.user.pk == int(user_pk):
+            request.user.is_verified = True
+            request.user.save()     # Если данные совпали - устанавливаем флаг is_verified и сохраняем
+            messages.success(request, 'Верификация пройдена ')
+            return redirect(reverse('users:profile'))
+        else:
+            # Если код и pk пользователя не совпадают с данными текущего пользователя - выдаем ошибку
+            messages.error(request, f"Верификация не пройдена, перейдите по ссылке из email ")
+    else:
+        # pk не число - выдаем ошибку
+        messages.error(request, f"Некорректные данные")
+
+    return render(request, 'users/verify_email.html')
+
+
+def get_verify(request):
+    """
+    Контроллер запроса на верификацию. Генерирует проверочный код
+    для пользователя, формирует ссылку для проверки.
+    """
+    # Получаем данные пользователя
+    user_pk = request.user.pk
+    user_mail = request.user.email
+    v_code = ''.join(str(randint(0, 9)) for _ in range(6))  # Генерируем проверочный код
+    request.user.verify_code = v_code
+    request.user.save()     # Запоминаем проверочный код и сохраняем в базе
+    send_mail(
+        subject='Пройдите верификацию, перейдите по ссылке:',
+        message=f'http://127.0.0.1:8000/users/register/verify?pk={user_pk}&code={v_code}',
+        from_email=EMAIL_HOST_USER,
+        recipient_list=[user_mail]
+    )       # Отправляем email с проверочной ссылкой пользователю
+    print(f'http://127.0.0.1:8000/users/register/verify?pk={user_pk}&code={v_code}')  # Дублируем ссылку в консоль
+    return redirect(reverse('users:profile'))
