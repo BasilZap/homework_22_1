8ea34c97956d7216b1009da1e103da8b46f38cf6
BasilZap <zapr9@yandex.ru>
2023-09-14 01:19:31 +0300

    В модель пользователя добавлены поля: is_verified и verify_code. Создана миграция

--- users/models.py
+++ users/models.py
@@ -10,5 +10,8 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='Страна', **NULLABLE)
 
+    verify_code = models.CharField(max_length=12, verbose_name='Код', **NULLABLE)
+    is_verified = models.BooleanField(default=False, verbose_name='Верифицирован', **NULLABLE)
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
